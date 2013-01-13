b2eed59bdf2b13ae72f3b976bc2bd883fb94a896
Jon Staley <jmstaley@gmail.com>
2013-01-13 03:03:43 +0000

    handle no results

--- train.py
+++ train.py
@@ -12,19 +12,29 @@ def get_next_departure():
     r = requests.get(DEPARTURES_URL)
     data = BeautifulSoup(r.content)
     row = data.select(".firstRow")
-    hour, minute = row[0].td.string.split(':')
-    next_train = datetime.combine(date.today(), time(int(hour), int(minute)))
-    status = row[0].select('.status')[0].string
+    status = 'None'
+    next_train = None
+    if row:
+        hour, minute = row[0].td.string.split(':')
+        next_train = datetime.combine(date.today(), time(int(hour), int(minute)))
+        status = row[0].select('.status')[0].string
     return next_train, status
 
 @app.route('/')
 def index():
     # get next train time
     next_train, status = get_next_departure()
-    time_to = relativedelta(next_train, datetime.now())
+    if next_train:
+        time_to = relativedelta(next_train, datetime.now())
+        time_of = next_train.strftime('%H:%M')
+        minutes_to = time_to.minutes
+    else:
+        minutes_to = '0'
+        time_of = 'No train'
+        
     return render_template('index.html', 
-                           train_time=next_train.strftime('%H:%M'),
-                           minutes_to=time_to.minutes,
+                           train_time=time_of,
+                           minutes_to=minutes_to,
                            status=status)
 
 if __name__ == '__main__':
